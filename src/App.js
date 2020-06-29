a10e34e7cb9cdde38c015a4039c3fe1411aec2d1
aahdee <adavey21@gmail.com>
2020-06-28 23:07:37 -0400

    started repo, added p5 functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import aahdeelogo from "./aahdeelogo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src={aahdeelogo} className="App-logo" alt="logo" />
+        <p className="Name">Aren E. Davey</p>
+        <p className="Bio">
+          Carnegie Mellon University BCSA Student <br />
+          2020 Processing Foundation Fellow <br /> Generative Artist <br />
+          Twitch Streamer
+          <br />
+          Shape Aficionado
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
+
+function Background() {}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import P5Sketch from "./p5sketch";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
+    <P5Sketch />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change

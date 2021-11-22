4fd3c7298aa2fbca3049c8bfefbf5d7789dda1e6
aahdee <adavey21@gmail.com>
2021-11-21 22:58:23 -0500

    Updates

---
+++ precache-manifest.a2f190f0567083ef4e705b3d4be75dfc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80caa3bbab643233fa37119708fc9108",
+    "url": "/aahdee.github.io/index.html"
+  },
+  {
+    "revision": "32326216fc7f0a4049bd",
+    "url": "/aahdee.github.io/static/css/main.ad6e8bbd.chunk.css"
+  },
+  {
+    "revision": "eae1ce067aeecea84502",
+    "url": "/aahdee.github.io/static/js/2.fb689fa5.chunk.js"
+  },
+  {
+    "revision": "a65ed81b8b223fa6124e8f70f4b5dbb3",
+    "url": "/aahdee.github.io/static/js/2.fb689fa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32326216fc7f0a4049bd",
+    "url": "/aahdee.github.io/static/js/main.55670a12.chunk.js"
+  },
+  {
+    "revision": "df9e1dd3d724efa03149",
+    "url": "/aahdee.github.io/static/js/runtime-main.e338bb9f.js"
+  },
+  {
+    "revision": "69abbbf7bf2a082655ed5504cfa5a42c",
+    "url": "/aahdee.github.io/static/media/aahdeelogo.69abbbf7.svg"
+  },
+  {
+    "revision": "ab8bef09ea74bd2a6a8e137a4051e805",
+    "url": "/aahdee.github.io/static/media/cubic-webfont.ab8bef09.woff2"
+  },
+  {
+    "revision": "afd9cb0e7b5f6837f2abddec1a3c8c68",
+    "url": "/aahdee.github.io/static/media/cubic-webfont.afd9cb0e.woff"
+  },
+  {
+    "revision": "314113d0570c59c22b93eb98ef5cb64c",
+    "url": "/aahdee.github.io/static/media/kubikmeta-webfont.314113d0.woff2"
+  },
+  {
+    "revision": "e6e68453c772badeb263257e586bc56c",
+    "url": "/aahdee.github.io/static/media/kubikmeta-webfont.e6e68453.woff"
+  },
+  {
+    "revision": "5927a4188e1efd8cc4da9aabc9a354fd",
+    "url": "/aahdee.github.io/static/media/prismfont_clv3-webfont.5927a418.woff"
+  },
+  {
+    "revision": "daaa325daf633ea1356a2e00962105e2",
+    "url": "/aahdee.github.io/static/media/prismfont_clv3-webfont.daaa325d.woff2"
+  }
+]);
